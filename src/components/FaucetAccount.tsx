f319fe9c2f74ea5fb168adedb09804dda4a5a874
catsoap <sjehann@gmail.com>
2020-09-22 23:08:45 +0200

    minifix display

--- src/components/FaucetAccount.tsx
+++ src/components/FaucetAccount.tsx
@@ -3,27 +3,28 @@ import { Box, Flex, Spinner, jsx } from "theme-ui";
 import DisplayAmount from "./DisplayAmount";
 
 function FaucetAccount(props: any) {
-    const { balance, isLoading, pkh, node } = props;
+  const { balance, isLoading, pkh, node } = props;
 
-    return isLoading ? (
-        <Spinner />
-    ) : (
-        <Flex
-            mb={2}
-            px={2}
-            sx={{
-                alignContent: "center",
-                justifyContent: "space-between",
-                backgroundColor: "secondary"
-            }}
-        >
-            <Box>{node}</Box>
-            <Box>{pkh}</Box>
-            <Box>
-                <DisplayAmount amount={balance} />
-            </Box>
-        </Flex>
-    );
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <Flex
+      mb={2}
+      px={2}
+      sx={{
+        flexDirection: ["column", "row"],
+        alignContent: "center",
+        justifyContent: "space-between",
+        backgroundColor: "secondary",
+      }}
+    >
+      <Box>{node}</Box>
+      <Box>{pkh}</Box>
+      <Box>
+        <DisplayAmount amount={balance} />
+      </Box>
+    </Flex>
+  );
 }
 
 export default FaucetAccount;
