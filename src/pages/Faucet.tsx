ae380bf1a70882eccd9050dc9d5a8314d76ac7a7
catsoap <sjehann@gmail.com>
2020-10-05 23:21:29 +0200

    display validation errors and info

--- src/pages/Faucet.tsx
+++ src/pages/Faucet.tsx
@@ -4,19 +4,52 @@ import { Tezos } from "@taquito/taquito";
 import { NetworkContext } from "../Context";
 import FaucetAccount from "../components/FaucetAccount";
 import useFaucet from "../hooks/useFaucet";
+import { Warning, Info } from "../components/Messages";
 
 export default function Faucet() {
   const { network } = useContext(NetworkContext);
   const { isLoading, pkh, balance, error } = useFaucet(network, true);
   const [toAddress, setToAddress] = useState("");
   const [amount, setAmount] = useState(25);
-  const [alertMsg, setAlertMsg] = useState("");
   const [transferRequested, setTransferRequested] = useState(false);
   const [balanceRefresh, setBalanceRefresh] = useState(false);
+  const [alertMsg, setAlertMsg] = useState("");
+  const [formErrors, setFormErrors] = useState([] as string[]);
+  const [info, setInfo] = useState("");
+
+  const showFormErrors = (e: Array<string>) =>
+    e.map((message, i) => (
+      <Warning title="Validation error" key={i}>
+        {message}
+      </Warning>
+    ));
+
+  const showInfo = (message: string) => <Info>{message}</Info>;
 
   if (error) {
     setAlertMsg(error);
   }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setFormErrors([]);
+
+    const errors = [];
+    if (validateAddress(toAddress) !== ValidationResult.VALID) {
+      errors.push("Please check the destination address");
+    }
+
+    if (isNaN(amount)) {
+      errors.push("Please enter a valid amount");
+    }
+
+    if (errors.length > 0) {
+      setFormErrors(errors);
+    } else {
+      setTransferRequested(true);
+      setInfo("Transfer operation pending...");
+    }
+  };
 
   useEffect(() => {
     async function transfer() {
@@ -25,12 +58,9 @@ export default function Faucet() {
           to: toAddress,
           amount: amount,
         });
-
-        setAlertMsg(`operation ${op.hash} in progress`);
-
+        setInfo(`operation ${op.hash} in progress`);
         await op.confirmation(1);
-
-        setAlertMsg(`operation ${op.hash} confirmed`);
+        setInfo(`operation ${op.hash} confirmed`);
         setBalanceRefresh(true);
       } catch (e) {
         setAlertMsg(`oops something bad happened: ${JSON.stringify(e)}`);
@@ -52,25 +82,17 @@ export default function Faucet() {
   ) : (
     <div>
       <FaucetAccount balance={balance} pkh={pkh} />
+      {formErrors.length > 0 && showFormErrors(formErrors)}
+      {info && showInfo(info)}
       {alertMsg && <p>{alertMsg}</p>}
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (validateAddress(toAddress) !== ValidationResult.VALID) {
-            setAlertMsg("Please check the destination address");
-          } else {
-            setTransferRequested(true);
-            setAlertMsg("Transfer operation pending...");
-          }
-        }}
-      >
+      <form onSubmit={handleSubmit}>
         <div>
           <label className="block">
             <span className="text-gray-700">Amount:</span>
             <input
               className="form-input mt-1 block w-full"
               id="amount"
-              defaultValue={amount}
+              defaultValue={25}
               onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                 setAmount(parseInt(e.target.value))
               }
